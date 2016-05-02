5d2ade011f63b404ecac33a2c5a0d34ba3a7f86d
azu <azuciao@gmail.com>
2016-05-02 19:40:47 +0900

    feat(preset): add textlint-rule-max-ten  to preset

--- packages/textlint-rule-preset-continuous-manual-writing/index.js
+++ packages/textlint-rule-preset-continuous-manual-writing/index.js
@@ -3,15 +3,19 @@
 module.exports = {
     rules: {
         "sentence-length": require("textlint-rule-sentence-length"),
-        "max-comma": require("textlint-rule-max-comma")
+        "max-comma": require("textlint-rule-max-comma"),
+        "max-ten": require("textlint-rule-max-ten")
     },
     rulesConfig: {
         // 文長は90文字以下とする
         "sentence-length": {
             max: 90
         },
-        // コンマは1文中に4つまで
-        "textlint-rule-max-comma":{
+        //コンマと読点は1文中に4つまで
+        "max-comma": {
+            max: 4
+        },
+        "max-ten": {
             max: 4
         }
     }
