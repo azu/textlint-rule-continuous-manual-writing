ed0a31baea00d5a33d5ffb177f47017e7905d9f2
azu <azuciao@gmail.com>
2016-05-02 19:46:28 +0900

    feat(preset): add conjunction rules to preset

--- packages/textlint-rule-preset-continuous-manual-writing/index.js
+++ packages/textlint-rule-preset-continuous-manual-writing/index.js
@@ -6,7 +6,9 @@ module.exports = {
         "max-comma": require("textlint-rule-max-comma"),
         "max-ten": require("textlint-rule-max-ten"),
         "no-double-negative-ja": require("textlint-rule-no-double-negative-ja"),
-        "no-mix-dearu-desumasu": require("textlint-rule-no-mix-dearu-desumasu")
+        "no-mix-dearu-desumasu": require("textlint-rule-no-mix-dearu-desumasu"),
+        "no-start-duplicated-conjunction": require("textlint-rule-no-start-duplicated-conjunction"),
+        "no-doubled-conjunction": require("textlint-rule-no-doubled-conjunction")
     },
     rulesConfig: {
         // 文長は90文字以下とする
@@ -23,6 +25,14 @@ module.exports = {
         // 二重否定は使用しない
         "no-double-negative-ja": true,
         // ですます調を使用する(「ですます」調と「である」調の混在させない)
-        "no-mix-dearu-desumasu": true
+        "no-mix-dearu-desumasu": true,
+        // 同じ表現から文を開始しすぎない
+        // - 同じ文から始めない
+        "no-start-duplicated-conjunction": {
+            "interval" : 2
+        },
+        // - 同じ接続詞を連続させない
+        "no-doubled-conjunction": true
+
     }
 };
