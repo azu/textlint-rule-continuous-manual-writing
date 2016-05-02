ad9edb77d973f27270359f92bd7e2136cea0bcd8
azu <azuciao@gmail.com>
2016-05-02 20:15:28 +0900

    feat(preset): add no-dropping-the-ra to preset

--- packages/textlint-rule-preset-continuous-manual-writing/index.js
+++ packages/textlint-rule-preset-continuous-manual-writing/index.js
@@ -8,7 +8,9 @@ module.exports = {
         "no-double-negative-ja": require("textlint-rule-no-double-negative-ja"),
         "no-mix-dearu-desumasu": require("textlint-rule-no-mix-dearu-desumasu"),
         "no-start-duplicated-conjunction": require("textlint-rule-no-start-duplicated-conjunction"),
-        "no-doubled-conjunction": require("textlint-rule-no-doubled-conjunction")
+        "no-doubled-conjunction": require("textlint-rule-no-doubled-conjunction"),
+        "no-dropping-the-ra": require("textlint-rule-no-dropping-the-ra")
+
     },
     rulesConfig: {
         // 文長は90文字以下とする
@@ -26,10 +28,12 @@ module.exports = {
         "no-double-negative-ja": true,
         // ですます調を使用する(「ですます」調と「である」調の混在させない)
         "no-mix-dearu-desumasu": true,
+        // 正式な送り仮名を使用する ら抜き言葉を使用しない
+        "no-dropping-the-ra": true,
         // 同じ表現から文を開始しすぎない
         // - 同じ文から始めない
         "no-start-duplicated-conjunction": {
-            "interval" : 2
+            "interval": 2
         },
         // - 同じ接続詞を連続させない
         "no-doubled-conjunction": true
