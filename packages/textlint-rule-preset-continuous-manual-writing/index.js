4d40e998001d8d93b8c2e16380b7bcc05ad3b3b4
azu <azuciao@gmail.com>
2016-05-02 19:39:09 +0900

    feat(preset): add textlint-rule-max-comma to preset

--- packages/textlint-rule-preset-continuous-manual-writing/index.js
+++ packages/textlint-rule-preset-continuous-manual-writing/index.js
@@ -2,11 +2,17 @@
 "use strict";
 module.exports = {
     rules: {
-        "sentence-length": require("textlint-rule-sentence-length")
+        "sentence-length": require("textlint-rule-sentence-length"),
+        "max-comma": require("textlint-rule-max-comma")
     },
     rulesConfig: {
+        // 文長は90文字以下とする
         "sentence-length": {
             max: 90
+        },
+        // コンマは1文中に4つまで
+        "textlint-rule-max-comma":{
+            max: 4
         }
     }
 };
