0c99140a33831a8c8f7a1a12c12e845c123e5c27
azu <azuciao@gmail.com>
2016-05-02 19:42:03 +0900

    feat(preset): add no-double-negative-ja  to preset

--- packages/textlint-rule-preset-continuous-manual-writing/index.js
+++ packages/textlint-rule-preset-continuous-manual-writing/index.js
@@ -4,7 +4,8 @@ module.exports = {
     rules: {
         "sentence-length": require("textlint-rule-sentence-length"),
         "max-comma": require("textlint-rule-max-comma"),
-        "max-ten": require("textlint-rule-max-ten")
+        "max-ten": require("textlint-rule-max-ten"),
+        "no-double-negative-ja": require("textlint-rule-no-double-negative-ja")
     },
     rulesConfig: {
         // 文長は90文字以下とする
@@ -17,6 +18,8 @@ module.exports = {
         },
         "max-ten": {
             max: 4
-        }
+        },
+        // 二重否定は使用しない
+        "no-double-negative-ja": true
     }
 };
