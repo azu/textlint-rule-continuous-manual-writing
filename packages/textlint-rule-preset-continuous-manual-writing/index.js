37d578dda8a4319abe282d327331642b9a5bfb25
azu <azuciao@gmail.com>
2016-05-03 21:34:07 +0900

    feat(preset): use jtf-style preset

--- packages/textlint-rule-preset-continuous-manual-writing/index.js
+++ packages/textlint-rule-preset-continuous-manual-writing/index.js
@@ -1,5 +1,6 @@
 // LICENSE : MIT
 "use strict";
+var jtfRules = require("textlint-rule-preset-jtf-style").rules;
 module.exports = {
     rules: {
         "sentence-length": require("textlint-rule-sentence-length"),
@@ -9,8 +10,10 @@ module.exports = {
         "no-mix-dearu-desumasu": require("textlint-rule-no-mix-dearu-desumasu"),
         "no-start-duplicated-conjunction": require("textlint-rule-no-start-duplicated-conjunction"),
         "no-doubled-conjunction": require("textlint-rule-no-doubled-conjunction"),
-        "no-dropping-the-ra": require("textlint-rule-no-dropping-the-ra")
-
+        "no-dropping-the-ra": require("textlint-rule-no-dropping-the-ra"),
+        "arabic-kanji-numbers": jtfRules["2.2.2.算用数字と漢数字の使い分け"],
+        "prefer-fullwidth-katakana": jtfRules["2.1.5.カタカナ"],
+        "no-space-between-halfwidth-characters": jtfRules["3.1.1.全角文字と半角文字の間"]
     },
     rulesConfig: {
         // 文長は90文字以下とする
